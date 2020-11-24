364818c4924854628e24f65f1e640a98ac894347
RahelUen <60141481+RahelUen@users.noreply.github.com>
2020-11-24 21:12:03 +0100

    alte commits
    die ca. 19.000 zusätzlichen Zeilen der package-lock.js wurden durch zweifacher Ausführung von "git reset HEAD^" und "Discard changes" der package-lock.js durchgeführt. Als nächstes muss getestet werden, ob "npm start" auf dem Microsoft Surface funktioniert.
    
    Co-Authored-By: Lukas Ulmer <64519364+LukasOrlando@users.noreply.github.com>
    Co-Authored-By: Sania-Zeidan <72863385+Sania-Zeidan@users.noreply.github.com>

---
+++ frontend/src/firebaseconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlZY6QdkrNaQodpr0lTcSYTQ6nHqzPHOI",
+    authDomain: "hdmanagement-39177.firebaseapp.com",
+    databaseURL: "https://hdmanagement-39177.firebaseio.com",
+    projectId: "hdmanagement-39177",
+    storageBucket: "hdmanagement-39177.appspot.com",
+    messagingSenderId: "230352853350",
+    appId: "1:230352853350:web:50af80e103c02bd91010a7"
+};
+
+export default firebaseConfig;
