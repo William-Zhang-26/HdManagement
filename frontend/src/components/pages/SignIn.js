17281204f22a2bc6561026bf47f9c96ad95c233e
Sania-Zeidan <72863385+Sania-Zeidan@users.noreply.github.com>
2020-11-25 21:35:04 +0100

    SignIn Draft
    Draft = Entwurf

--- frontend/src/components/pages/SignIn.js
+++ frontend/src/components/pages/SignIn.js
@@ -1,9 +1,45 @@
 import React, {Component} from 'react';
-import Button from '@material-ui/core/Button';
+import {Button, Grid, Typography, withStyles}from '@material-ui/core';
 
  
+class SignIn extends Component {
+  render() {
+    const { classes } = this.props;
+    
+    return (
+      <div>
+        <Typography className={classes.root} align='center' variant='h6'>Welcome to the HdM React/Python Project Showcase</Typography>
+        <Typography className={classes.root} align='center'>It appears, that you are not signed in.</Typography>
+        <Typography className={classes.root} align='center'>To use the services of the HdM Bank please</Typography>
+        <Grid container justify='center'>
+          <Grid item>
+            <Button variant='contained' color='primary' onClick={this.handleSignInButtonClicked}>
+              Sign in with Google
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 
+const styles = theme => ({
+	root: {
+		margin: theme.spacing(2)
+	}
+});
+
+
+export default withStyles(styles)(SignIn)
+
+
+
+
+
+
+
+/*
 
 class SignIn extends Component{
 
@@ -29,10 +65,6 @@ export default (SignIn)
 
 
 
-
-
-
-/*
 function App() {
   return (
     <Button variant="contained" color="primary">
@@ -43,3 +75,4 @@ function App() {
  
 ReactDOM.render(<App />, document.querySelector('#app'));
 */
+
