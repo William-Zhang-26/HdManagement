2dcdd1d9ac06732ca1d60403b33bd466266a8c2c
Lukas Ulmer <64519364+LukasOrlando@users.noreply.github.com>
2020-12-10 21:28:28 +0100

    Bug Fixes
    Co-Authored-By: Sania-Zeidan <72863385+Sania-Zeidan@users.noreply.github.com>
    Co-Authored-By: RahelUen <60141481+RahelUen@users.noreply.github.com>

--- frontend/src/components/dialogs/ContextErrorMessage.js
+++ frontend/src/components/dialogs/ContextErrorMessage.js
@@ -4,6 +4,7 @@ import { withStyles, Button } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
 import AlertTitle from '@material-ui/lab/AlertTitle';
 import AutorenewIcon from '@material-ui/icons/Autorenew';
+import PropTypes from 'prop-types';
 
 
 class ContextErrorMessage extends Component {
@@ -50,6 +51,23 @@ const styles = theme => ({
   }
 });
 
+/** PropTypes */ 
+ContextErrorMessage.propTypes = {
+  /** @ignore */
+  classes: PropTypes.object.isRequired,
+  /** 
+   * The error object, which drives the error message 
+   * If not null, the error message is shown 
+   */
+  error: PropTypes.object,
+  /**  A contextual error message to be shown */
+  contextErrorMsg: PropTypes.string,
+  /** 
+   * A reload handler for the onReload event, which occurs if the reload button is clicked. 
+   * If given a reload button is shown 
+   */
+  onReload: PropTypes.func
+}
 
 
 
