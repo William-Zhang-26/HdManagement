8f00515dd57cba085d48b1ab3e72feb87e05500d
Sania-Zeidan <72863385+Sania-Zeidan@users.noreply.github.com>
2021-01-05 19:40:22 +0100

    Projekte löschen
    Co-Authored-By: RahelUen <60141481+RahelUen@users.noreply.github.com>
    Co-Authored-By: Lukas Ulmer <64519364+LukasOrlando@users.noreply.github.com>

--- frontend/src/components/dialogs/ProjectDeleteDialog.js
+++ frontend/src/components/dialogs/ProjectDeleteDialog.js
@@ -1,1 +1,109 @@
 //Entfernen eines Projektes
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, Button, IconButton, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import ProjectAPI  from '../api/ProjectAPI';
+import ContextErrorMessage from './ContextErrorMessage';
+import LoadingProgress from './LoadingProgress';
+
+
+class ProjectDeleteDialog extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Init the state
+    this.state = {
+      deletingInProgress: false,
+      deletingError: null
+    };
+  }
+
+  /** Delete the Project */
+  deleteProject = () => {
+    ProjectAPI.getAPI().deleteProject(this.props.project.getID()).then(project => {
+      this.setState({
+        deletingInProgress: false,              // disable loading indicator  
+        deletingError: null                     // no error message
+      });
+      this.props.onClose(this.props.project);  // call the parent with the deleted customer
+    }).catch(e =>
+      this.setState({
+        deletingInProgress: false,              // disable loading indicator 
+        deletingError: e                        // show error message
+      })
+    );
+
+    // set loading to true
+    this.setState({
+      deletingInProgress: true,                 // show loading indicator
+      deletingError: null                       // disable error message
+    });
+  }
+
+  /** Handles the close / cancel button click event */
+  handleClose = () => {
+    // console.log(event);
+    this.props.onClose(null);
+  }
+
+  /** Renders the component */
+  render() {
+    const { classes, project, show } = this.props;
+    const { deletingInProgress, deletingError } = this.state;
+
+    return (
+      show ?
+        <Dialog open={show} onClose={this.handleClose}>
+          <DialogTitle id='delete-dialog-title'>Löschen des Projektes
+            <IconButton className={classes.closeButton} onClick={this.handleClose}>
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Sind Sie sicher, dass Sie das Projekt '{project.getName()} ' (ID: {project.getID()}) löschen möchten?
+            </DialogContentText>
+            <LoadingProgress show={deletingInProgress} />
+            <ContextErrorMessage error={deletingError} contextErrorMsg={`The project '${project.getName()} '(ID: ${project.getID()}) could not be deleted.`}
+              onReload={this.deleteProject} />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color='secondary'>
+              Abbrechen
+            </Button>
+            <Button variant='contained' onClick={this.deleteProject} color='primary'>
+              Löschen
+            </Button> 
+          </DialogActions>
+        </Dialog>
+        : null
+    );
+  }
+}
+
+/** Component specific styles */
+const styles = theme => ({
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  }
+});
+
+/** PropTypes */
+ProjectDeleteDialog.propTypes = {
+  /** @ignore */
+  classes: PropTypes.object.isRequired,
+
+  project: PropTypes.object.isRequired,
+
+  show: PropTypes.bool.isRequired,
+ 
+  onClose: PropTypes.func.isRequired,
+}
+
+export default withStyles(styles)(ProjectDeleteDialog);
