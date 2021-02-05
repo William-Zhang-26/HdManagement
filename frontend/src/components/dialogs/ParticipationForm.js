a8e7ee7d7de03231aa9747cd4f2b6db24b6a1dcc
RahelUen <60141481+RahelUen@users.noreply.github.com>
2021-02-05 18:53:59 +0100

    Teilnehmer hinzufügen
    Co-Authored-By: Sania-Zeidan <72863385+Sania-Zeidan@users.noreply.github.com>
    Co-Authored-By: Lukas Ulmer <64519364+LukasOrlando@users.noreply.github.com>

--- frontend/src/components/dialogs/ParticipationForm.js
+++ frontend/src/components/dialogs/ParticipationForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, Button, IconButton, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField } from '@material-ui/core';
-import { MenuItem, FormControl, InputLabel, Select, Typography, Grid} from '@material-ui/core';
+import { MenuItem, FormControl, InputLabel, Select, Typography, Grid } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import ProjectAPI  from '../../api/ProjectAPI';
 import ParticipationBO  from '../../api/ParticipationBO';
@@ -94,6 +94,12 @@ addParticipation = () => {
   } 
 
 
+  handleChange = (event) => {
+    this.setState({
+        module_id: event.target.value
+    });}
+
+
 
 /** Renders the component */
 render() {
@@ -112,7 +118,7 @@ render() {
   let header = 'Füllen Sie das Formular aus';
 
   return (
-    show ?
+    show && project ?
       <Dialog open={show} onClose={this.handleClose} maxWidth='xs'>
         <DialogTitle id='form-dialog-title'>{title}
           <IconButton className={classes.closeButton} onClick={this.handleClose}>
@@ -124,7 +130,183 @@ render() {
             {header}
           </DialogContentText>
           <form className={classes.root} noValidate autoComplete='off'>
-          <TextField autoFocus type='number' required fullWidth margin='normal' id='module_id' label='module_id:' value={module_id} onChange={this.textFieldValueChange} />
+          
+          { project.getAssignmentID() === 1 ?
+          <Grid>
+            <Typography>Modul</Typography>
+              <FormControl className={classes.formControl}>
+                  <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                  <Select
+                    value={module_id}
+                    onChange={this.handleChange}
+                  > 
+                    <MenuItem value={1}>338005</MenuItem>
+                    <MenuItem value={2}>338006</MenuItem>
+                    <MenuItem value={3}>338007</MenuItem>
+                    <MenuItem value={4}>338008</MenuItem>
+                    <MenuItem value={5}>338009</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            : project.getAssignmentID() === 2 ?
+            <Grid>
+            <Typography>Modul</Typography>
+              <FormControl className={classes.formControl}>
+                  <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                  <Select
+                    value={module_id}
+                    onChange={this.handleChange}
+                  > 
+                    <MenuItem value={6}>338010</MenuItem>
+                    <MenuItem value={7}>338011</MenuItem>
+                    <MenuItem value={8}>338012</MenuItem>
+                    <MenuItem value={9}>338013</MenuItem>
+                    <MenuItem value={10}>338014</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              : project.getAssignmentID() === 3 ?
+              <Grid>
+              <Typography>Modul</Typography>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                    <Select
+                      value={module_id}
+                      onChange={this.handleChange}
+                    > 
+                      <MenuItem value={11}>338015</MenuItem>
+                      <MenuItem value={12}>338016</MenuItem>
+                      <MenuItem value={13}>338017</MenuItem>
+                      <MenuItem value={14}>338018</MenuItem>
+                      <MenuItem value={15}>338019</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                : project.getAssignmentID() === 4 ?
+                <Grid>
+                <Typography>Modul</Typography>
+                  <FormControl className={classes.formControl}>
+                      <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                      <Select
+                        value={module_id}
+                        onChange={this.handleChange}
+                      > 
+                        <MenuItem value={16}>338020</MenuItem>
+                        <MenuItem value={17}>338021</MenuItem>
+                        <MenuItem value={18}>338022</MenuItem>
+                        <MenuItem value={19}>338023</MenuItem>
+                        <MenuItem value={20}>338024</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  : project.getAssignmentID() === 5 ?
+                  <Grid>
+                  <Typography>Modul</Typography>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                        <Select
+                          value={module_id}
+                          onChange={this.handleChange}
+                        > 
+                          <MenuItem value={21}>338025</MenuItem>
+                          <MenuItem value={22}>338026</MenuItem>
+                          <MenuItem value={23}>338027</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                    : project.getAssignmentID() === 6 ?
+                    <Grid>
+                    <Typography>Modul</Typography>
+                      <FormControl className={classes.formControl}>
+                          <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                          <Select
+                            value={module_id}
+                            onChange={this.handleChange}
+                          > 
+                            <MenuItem value={24}>338028</MenuItem>
+                            <MenuItem value={25}>338029</MenuItem>
+                            <MenuItem value={26}>338030</MenuItem>
+                          </Select>
+                        </FormControl>
+                      </Grid>
+                      : project.getAssignmentID() === 7 ?
+                      <Grid>
+                      <Typography>Modul</Typography>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                            <Select
+                              value={module_id}
+                              onChange={this.handleChange}
+                            > 
+                              <MenuItem value={27}>338031</MenuItem>
+                              <MenuItem value={28}>338032</MenuItem>
+                              <MenuItem value={29}>338033</MenuItem>
+                            </Select>
+                          </FormControl>
+                        </Grid>
+                        : project.getAssignmentID() === 8 ?
+                        <Grid>
+                        <Typography>Modul</Typography>
+                          <FormControl className={classes.formControl}>
+                              <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                              <Select
+                                value={module_id}
+                                onChange={this.handleChange}
+                              > 
+                                <MenuItem value={30}>338034</MenuItem>
+                                <MenuItem value={31}>338035</MenuItem>
+                                <MenuItem value={32}>338036</MenuItem>
+                              </Select>
+                            </FormControl>
+                          </Grid>
+                          : project.getAssignmentID() === 9 ?
+                          <Grid>
+                          <Typography>Modul</Typography>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                                <Select
+                                  value={module_id}
+                                  onChange={this.handleChange}
+                                > 
+                                  <MenuItem value={33}>338037</MenuItem>
+                                  <MenuItem value={34}>338038</MenuItem>
+                                  <MenuItem value={35}>338039</MenuItem>
+                                </Select>
+                              </FormControl>
+                            </Grid>
+                            : project.getAssignmentID() === 10 ?
+                            <Grid>
+                            <Typography>Modul</Typography>
+                              <FormControl className={classes.formControl}>
+                                  <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                                  <Select
+                                    value={module_id}
+                                    onChange={this.handleChange}
+                                  > 
+                                    <MenuItem value={36}>338040</MenuItem>
+                                    <MenuItem value={37}>338041</MenuItem>
+                                    <MenuItem value={38}>338042</MenuItem>
+                                  </Select>
+                                </FormControl>
+                              </Grid>
+                              : project.getAssignmentID() === 11 ?
+                              <Grid>
+                              <Typography>Modul</Typography>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel id="open-select-label">Bitte auswählen</InputLabel>
+                                    <Select
+                                      value={module_id}
+                                      onChange={this.handleChange}
+                                    > 
+                                      <MenuItem value={39}>338043</MenuItem>
+                                      <MenuItem value={40}>338044</MenuItem>
+                                      <MenuItem value={41}>338045</MenuItem>
+                                      <MenuItem value={42}>338046</MenuItem>
+                                    </Select>
+                                  </FormControl>
+                                </Grid>
+                                : null }
+
 
           <TextField autoFocus type='number' required fullWidth margin='normal' id='student_id' label='student_id:' value={student_id} onChange={this.textFieldValueChange} />
           </form>
